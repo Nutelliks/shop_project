71a54ac5df652adfc920e34c8126b653f70f6202
Nurislam Audangaliev <naudangaliev@gmailcom>
2025-12-21 13:31:40 +0500

    Static (css, js, ...) is ready and links is active

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 app_name = 'main'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+
 ]
--- main/views.py
+++ main/views.py
@@ -3,6 +3,26 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home - Main page'
+        'title': 'Home - Main page',
+        'content': 'Магазин мебели HOME'
     }
-    render(request, 'main/index.html', context)
+
+    return render(request, 'main/index.html', context)
+
+
+def about(request):
+    context = {
+        'title': 'Home - About page',
+        'content': 'О нас'
+    }
+
+    return render(request, 'main/about.html', context)
+
+
+def contact(request):
+    context = {
+        'title': 'Home - Contact page',
+        'content': 'Контактная инфо'
+    }
+
+    return render(request, 'main/contact.html', context)
