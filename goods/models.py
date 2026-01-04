4bcbe44413ce8013d8c3ab9deefa1923354ba5c9
Nurislam Audangaliev <naudangaliev@gmailcom>
2026-01-04 14:41:56 +0500

    Admin panel setup

--- goods/models.py
+++ goods/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 
 
 class Categories(models.Model):
@@ -36,10 +36,14 @@ class Products(models.Model):
 
     def __str__(self):
         return self.name
-    
 
+    def get_absolute_url(self):
+        return reverse("catalog:product", kwargs={"product_slug": self.slug})
+    
     def display_id(self):
         return f"{self.id:05}"
+    
+    
     
     def sell_price(self):
         if self.discount:
