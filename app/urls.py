48ae7dc9485313edfe17aa7a37a904f450767742
Nurislam Audangaliev <naudangaliev@gmailcom>
2026-02-07 17:07:54 +0500

    simplejwt for jwt auth

--- app/urls.py
+++ app/urls.py
@@ -20,7 +20,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from debug_toolbar.toolbar import debug_toolbar_urls
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from drf_spectacular.views import (
+    SpectacularAPIView, SpectacularSwaggerView)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView, TokenVerifyView)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -34,7 +38,9 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
-
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 
 
